36427f3f1100956af162c0cd6a744ff0e016ba20
Gaston <gastonpaez954@gmail.com>
2025-05-17 19:18:24 -0300

    eliminacion de archivos

--- backend/src/modules/autor/autor.model.ts
+++
@@ -1,7 +0,0 @@
-import { DataTypes, Model, Optional } from 'sequelize';
-import { sequelize } from 'src/database/config/db-config';
-
-interface AutorAttributes {
-    id: number;
-    name: string;
-}
