0f7d74df1f45ab0bd520a61cec7f2b1e9462186b
Lucas Frey <lucasfrey19@gmail.com>
2025-06-04 01:22:47 -0300

    del

--- frontend/src/API/interface/apiClient.ts
+++
@@ -1,7 +0,0 @@
-export interface APIClient<T> {
-    getAll(): Promise<T[]>;
-    getOne(id: number): Promise<T>;
-    create(data: Partial<T>): Promise<T>;
-    update(id: number, data: Partial<T>): Promise<T>;
-    delete(id: number): Promise<void>;
-}
