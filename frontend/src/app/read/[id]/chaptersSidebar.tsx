e6a4341977afc88ee426fc091cad6cfc0718dfae
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2025-06-07 21:20:35 -0300

    implementado logica y css de sidebar lectura

--- frontend/src/app/read/[id]/chaptersSidebar.tsx
+++ frontend/src/app/read/[id]/chaptersSidebar.tsx
@@ -1,23 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Styles from "./styles.module.css";
-export function ChaptersSideBar(chapters: string[]): React.JSX.Element {
-  
+import { TiChevronLeft } from "react-icons/ti";
+
+export function ChaptersSideBar(chapters: string[] ):React.JSX.Element {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="sidebar">
-        <div className="sidebar-right">
-            <div className="sidebar-toggle">
-                <button className="toggle-button">Toggle Chapters</button>
-            </div>
-        </div>
-        <div className="sidebar-content">
-            {
-                chapters.map((chapter, index) => (
-                <div key={index} className="chapter-header">
-                    <a href={`#chapter-${index + 1}`}>{chapter}</a>
-                </div>
-                ))
-            }
-        </div>
+    <div className={`${Styles.sidebar} ${open ? Styles.open : ""}`}>
+      <div className={Styles.sidebarToggle} onClick={() => setOpen((prev) => !prev)}>
+        <TiChevronLeft style={{ transform: open ? "rotate(180deg)" : "none", transition: "transform 0.3s" }} />
+      </div>
+      <div className={`${Styles.sidebarContent} ${open ? Styles.open : ""}`}>
+        {chapters.map((chapter, index) => (
+          <div key={index} className={Styles.chapterHeader}>
+            <a href={`#chapter-${index + 1}`}>{chapter}</a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
