44d7f2058cf4950a60082ed0554b9a84d2bc796f
Lucas Frey <lucasfrey19@gmail.com>
2025-06-17 17:38:19 -0300

    Log Book_genres service

--- backend/src/modules/book_genre/book_genres.service.ts
+++ backend/src/modules/book_genre/book_genres.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { BookGenre } from 'src/entidades/book_genres.entity';
@@ -7,6 +7,7 @@ import { BookGenreDto } from './book_genre.dto';
 
 @Injectable()
 export class BookGenresService {
+  private readonly logger = new Logger(BookGenresService.name);
   constructor(
     @InjectRepository(BookGenre)
     private bookGenreRepository: Repository<BookGenre>,
@@ -15,15 +16,17 @@ export class BookGenresService {
   ) {}
 
   async findAll(): Promise<BookGenre[]> {
+    this.logger.log('Listado de Generos de Libro Obtenido');
     return this.bookGenreRepository.find();
   }
   async create(data: Partial<BookGenreDto>): Promise<BookGenre> {
     const genre = await this.genreRepository.findOne({ where: { name: data.name } });
-    const newGenre = this.bookGenreRepository.create({
-      id_genre: genre?.id,id_book: data.id_book});
+    const newGenre = this.bookGenreRepository.create({id_genre: genre?.id,id_book: data.id_book});
+    this.logger.log('Genero de Libro Creado');
     return this.bookGenreRepository.save(newGenre);
   }
   async delete(id: number) {
+    this.logger.log('Genero de Libro Eliminado');
     return this.bookGenreRepository.delete(id);
   }
 }
