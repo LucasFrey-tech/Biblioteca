2afcaa92633fc79a8027e2850f67dd0cd23c4c00
Lucas Frey <lucasfrey19@gmail.com>
2025-06-17 17:41:09 -0300

    Log subscription_user service

--- backend/src/modules/subscriptions/subscription_user.service.ts
+++ backend/src/modules/subscriptions/subscription_user.service.ts
@@ -1,11 +1,12 @@
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException, Logger } from "@nestjs/common";
 import { UserSubscription } from "../../entidades/subscription_user.entity";
 import { User } from "../../entidades/user.entity";
 
 @Injectable()
 export class UserSubscriptionService {
+  private readonly logger = new Logger(UserSubscriptionService.name);
   constructor(
     @InjectRepository(UserSubscription)
     private userSubscriptionRepository: Repository<UserSubscription>,
@@ -13,13 +14,10 @@ export class UserSubscriptionService {
     private userRepository: Repository<User>,
   ) {}
 
-  async createSubscription(
-    userId: number,
-    startDate: Date,
-    endDate: Date,
-  ): Promise<UserSubscription> {
+  async createSubscription(userId: number, startDate: Date, endDate: Date): Promise<UserSubscription> {
     const user = await this.userRepository.findOne({ where: { id: userId } });
     if (!user) {
+      this.logger.log('Usuario No Encontrado');
       throw new NotFoundException(`User with ID ${userId} not found`);
     }
 
@@ -29,10 +27,12 @@ export class UserSubscriptionService {
     subscription.endDate = endDate;
     subscription.ongoing = true;
 
+    this.logger.log('Suscripción Creada');
     return this.userSubscriptionRepository.save(subscription);
   }
 
   async getUserSubscriptions(userId: number): Promise<UserSubscription[]> {
+    this.logger.log('Obtener Suscripción de Usuario por ID');
     return this.userSubscriptionRepository.find({
       where: { idUser: userId },
       relations: ['user'],
@@ -40,13 +40,14 @@ export class UserSubscriptionService {
   }
 
   async cancelSubscription(id: number): Promise<void> {
-    const subscription = await this.userSubscriptionRepository.findOne({
-      where: { id },
-    });
+    const subscription = await this.userSubscriptionRepository.findOne({where: { id }});
     if (!subscription) {
+      this.logger.log('Suscripción No Encontrada');
       throw new NotFoundException(`Subscription with ID ${id} not found`);
     }
     subscription.ongoing = false;
+
+    this.logger.log('Subscripcion Cancelada');
     await this.userSubscriptionRepository.save(subscription);
   }
 }
