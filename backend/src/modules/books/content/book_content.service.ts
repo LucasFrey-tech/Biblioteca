69791b437745c0de4f103f5f104c36d122a4dec5
Lucas Frey <lucasfrey19@gmail.com>
2025-06-17 17:39:12 -0300

    Log book_content service

--- backend/src/modules/books/content/book_content.service.ts
+++ backend/src/modules/books/content/book_content.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { VirtualBookContent } from 'src/entidades/virtual_book_content.entity';
 import { BookContentDTO } from './book_content.dto';
+import { log } from 'console';
 
 @Injectable()
 export class BookContentService {
+  private readonly logger = new Logger(BookContentService.name);
   constructor(
     @InjectRepository(VirtualBookContent)
     private bookContentRepository: Repository<VirtualBookContent>,
@@ -13,7 +15,12 @@ export class BookContentService {
 
   async get(id:number):Promise<BookContentDTO|null>{
     const bookContent = await this.bookContentRepository.findOne({ where: {idBook: id} })
-    if(!bookContent) return null
+    if(!bookContent) {
+      this.logger.log('Contenido No Encontrado');
+      return null
+    } 
+      
+    this.logger.log('Contenido Encontrado');
     return {
       idBook: bookContent?.idBook,
       content: bookContent?.content
@@ -21,15 +28,18 @@ export class BookContentService {
   }
 
   create(bookContent: Partial<BookContentDTO>): Promise<BookContentDTO> {
-      const newGenre = this.bookContentRepository.create(bookContent);
-      return this.bookContentRepository.save(newGenre);
+    const newGenre = this.bookContentRepository.create(bookContent);
+    this.logger.log('Contenido Creado');
+    return this.bookContentRepository.save(newGenre);
   }
 
   update(id:number, bookContent: BookContentDTO){
+    this.logger.log('Contenido Actualizado');
     this.bookContentRepository.update(id,bookContent)
   }
 
   delete(id: number) {
+    this.logger.log('Contenido Borrado');
     return this.bookContentRepository.delete(id);
   }
 
