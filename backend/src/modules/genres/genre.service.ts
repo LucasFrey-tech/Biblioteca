844c2f164d3c3d902ea5b7e8c55d88aead97bdb1
Lucas Frey <lucasfrey19@gmail.com>
2025-06-17 17:40:31 -0300

    Log genre service

--- backend/src/modules/genres/genre.service.ts
+++ backend/src/modules/genres/genre.service.ts
@@ -1,21 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Genre } from 'src/entidades/genre.entity';
 
 @Injectable()
 export class GenresService {
+  private readonly logger = new Logger(GenresService.name);
   constructor(
     @InjectRepository(Genre)
     private genreRepository: Repository<Genre>,
   ) {}
 
   findAll(): Promise<Genre[]> {
+    this.logger.log('Lista de Generos Obtenida');
     return this.genreRepository.find();
   }
 
   create(genre: Partial<Genre>): Promise<Genre> {
     const newGenre = this.genreRepository.create(genre);
+    this.logger.log('Genero Creado');
     return this.genreRepository.save(newGenre);
   }
 }
