986de168f0b3ac53da4b97e7c067b5371e1a3930
Lucas Frey <lucasfrey19@gmail.com>
2025-06-17 17:40:51 -0300

    Log shopping_cart service

--- backend/src/modules/shopping_cart/shopping_cart.service.ts
+++ backend/src/modules/shopping_cart/shopping_cart.service.ts
@@ -1,5 +1,5 @@
 import { Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ShoppingCartBook } from '../../entidades/shopping_cart_book.entity';
 import { Author } from 'src/entidades/author.entity';
@@ -9,6 +9,7 @@ import { BookCartDTO } from './book_cart.dto';
 
 @Injectable()
 export class ShoppingCartService {
+    private readonly logger = new Logger(ShoppingCartBook.name);
     constructor(
         @InjectRepository(ShoppingCartBook)
         private cartBookShopingRepository: Repository<ShoppingCartBook>,
@@ -25,12 +26,19 @@ export class ShoppingCartService {
 
     async findByUser(idUser: number): Promise<BookCartDTO[] | null> {
         const cartItems = await this.cartBookShopingRepository.find({ where: { idUser } });
-        if (!cartItems.length) return null;
+        if (!cartItems.length) {
+            this.logger.log('Carrito Vacio');
+            return null;
+        } 
 
         const results = await Promise.all(
             cartItems.map(async (cart) => {
                 const book = await this.booksRepository.findOne({ where: { id: cart.idBook } });
-                if (!book) return null;
+                if (!book) {
+                    this.logger.log('Libro No Encontrado');
+                    return null;
+                } 
+                    
 
                 const author = await this.authorRepository.findOne({ where: { id: book.author_id } });
 
@@ -46,20 +54,23 @@ export class ShoppingCartService {
                 );
             })
         );
-
+        this.logger.log('Carrito Obtenido');
         return results.filter((item): item is BookCartDTO => item !== null);
     }
 
     create(book: Partial<ShoppingCartBook>) {
+        this.logger.log('Carrito Creado');
         return this.cartBookShopingRepository.save(book);
     }
 
     async update(idBookCart: number, updateData: Partial<ShoppingCartBook>) {
         await this.cartBookShopingRepository.update(idBookCart, updateData);
+        this.logger.log('Carrito Actualizado');
         return this.cartBookShopingRepository.find({where : {id: idBookCart}});
     }
 
     delete(id: number) {
+        this.logger.log('Carrito Borrado');
         return this.cartBookShopingRepository.delete(id);
     }
 }
