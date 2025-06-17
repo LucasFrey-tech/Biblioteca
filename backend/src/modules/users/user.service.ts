49a24d387b45e97eb093966731d99b4bce995bf3
Lucas Frey <lucasfrey19@gmail.com>
2025-06-17 17:41:19 -0300

    Log user service

--- backend/src/modules/users/user.service.ts
+++ backend/src/modules/users/user.service.ts
@@ -1,5 +1,5 @@
 import * as bcrypt from 'bcrypt';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ILike, Repository } from 'typeorm';
@@ -7,12 +7,14 @@ import { User } from '../../entidades/user.entity';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) { }
 
   async findAll(search = ''): Promise<User[]> {
+    this.logger.log('Lista de Usuarios Obtenida');
     return this.usersRepository.find({
       where: [
         { firstname: ILike(`%${search}%`) },
@@ -24,6 +26,7 @@ export class UsersService {
   }
 
   findOne(id: number) {
+    this.logger.log('Usuario Encontrado');
     return this.usersRepository.findOne({ where: { id } });
   }
 
@@ -32,23 +35,30 @@ export class UsersService {
     const existingUserName = await this.usersRepository.findOne({ where: { username: user.username } });
     // Si el que registra es tanto un mail como un username
     if (existingUserEmail && existingUserName) {
+      this.logger.log('Correo y Nombre de Usuario ya registrado');
       throw new BadRequestException('El correo y el nombre de usuario ya están registrados');
     }
     // Caso de que el usuario ingrese el mismo mail
     if (existingUserEmail) {
+      this.logger.log('Correo ya Registrado');
       throw new BadRequestException('El correo ya está registrado');
     }
     // Caso de que el usuario ingrese el mismo nombre de usuario
     if (existingUserName) {
+      this.logger.log('Nombre de Usuario ya registrado');
       throw new BadRequestException('Este nombre de usuario ya existe');
     }
+
+    this.logger.log('Cuenta de Usuario Creada');
     return this.usersRepository.save(user);
   }
 
   async findByEmail(email: string): Promise<User | null> {
+    this.logger.log('Usuario Encontrado por Email');
     return this.usersRepository.findOne({ where: { email } });
   }
   async findByUser(username: string): Promise<User | null> {
+    this.logger.log('Usuario Encontrado por Nombre de Usuario');
     return this.usersRepository.findOne({ where: { username } });
   }
 
@@ -57,11 +67,15 @@ export class UsersService {
       const saltRounds = 10;
       updateData.password = await bcrypt.hash(updateData.password, saltRounds);
     }
+
+    this.logger.log('Usuario Actualizado');
+
     await this.usersRepository.update(id, updateData);
     return this.findOne(id);
   }
 
   delete(id: number) {
+    this.logger.log('Usuario Borrado')
     return this.usersRepository.delete(id);
   }
 }
