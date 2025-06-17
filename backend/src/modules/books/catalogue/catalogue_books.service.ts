de440ac47fbaf794f1e754a946402a533adbf357
Lucas Frey <lucasfrey19@gmail.com>
2025-06-17 17:39:00 -0300

    Log catalogue_book service

--- backend/src/modules/books/catalogue/catalogue_books.service.ts
+++ backend/src/modules/books/catalogue/catalogue_books.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { BooksService } from '../book/book.service';
 import { CatalogueBookDTO } from './catalogue_book.dto';
 
 @Injectable()
 export class CatalogueBooksService {
+    private readonly logger = new Logger(CatalogueBooksService.name)
     constructor(private readonly booksService: BooksService) { }
 
     async findAll(): Promise<CatalogueBookDTO[]> {
         return this.booksService.findAll().then(books =>
             books.map(book => {
+                this.logger.log('Listado de Libros del Catalogo Obtenido');
                 return new CatalogueBookDTO(book.id, book.title, book.author, book.author_id, book.description, book.genre, book.anio, book.image, book.stock, book.subscriber_exclusive, book.price);
             })
         );
@@ -17,8 +19,11 @@ export class CatalogueBooksService {
     async findOne(id: number) {
         return this.booksService.findOne(id).then(book => {
             if (!book) {
+                this.logger.log('Libro No Encontrado');
                 return null;
             }
+
+            this.logger.log('Libro Encontrado');
             return new CatalogueBookDTO(book.id, book.title, book.author, book.author_id, book.description, book.genre, book.anio, book.image, book.stock, book.subscriber_exclusive, book.price);
         })
     }
