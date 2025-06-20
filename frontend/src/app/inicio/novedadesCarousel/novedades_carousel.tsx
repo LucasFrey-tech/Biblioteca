1d25a4c9a5884c196356d6bf1a7ce8553cd6879e
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2025-06-20 18:34:06 -0300

    carousel front implement back

--- frontend/src/app/inicio/novedadesCarousel/novedades_carousel.tsx
+++ frontend/src/app/inicio/novedadesCarousel/novedades_carousel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Carousel,
   CarouselContent,
@@ -7,40 +7,46 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import Styles from './styles.module.css';
+import { CarouselItemDTO } from "@/API/types/carousel.dto";
+import { BaseApi } from "@/API/baseApi";
+import { useRouter } from 'next/navigation';
 
 
 export default function NovedadesCarousel(): React.JSX.Element {
+    const [carouselItems, setCarouselItems] = useState<CarouselItemDTO[]>([]);
+    const router = useRouter();
+    const apiRef = useRef(new BaseApi());
+    
+  useEffect(() => {
+    async function getCarouselItems() {
+      const carouselItems = await apiRef.current.carousel.getAll();
+      console.log(carouselItems)
+      setCarouselItems(carouselItems);
+    }
+    getCarouselItems()
+  }, []);
 
-    return (
-        <div className={Styles.novedadesCarousel}>
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true
-          }}
-          className="max-w"
-          >
-          <CarouselContent>
-            {Array.from({ length: 4 }).map((_, index) => (
-              <CarouselItem key={index} className="md:basis-1/1 lg:basis-1/1">
-                <div className={Styles.novedadesBookCard}>
-                  <img src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-180680638-676f621f720bc.jpg?crop=1.00xw:0.752xh;0,0.118xh&resize=1200:*"}/>
-                  <div className={Styles.details}>
-                    <div className={Styles.detailsTop}/>
-                    <div className={Styles.detailsMid}>
-                      <h2>PERROS: La ultima frontera?</h2>
-                    </div>
-                    <div className={Styles.detailsBot}>
-                      <p>Un analisis instrospectivo sobre la dieta canina, y las consecuencia sobre la relaciones gatunas.</p>
-                    </div>
-                  </div>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious/> 
-          <CarouselNext /> 
-        </Carousel>
-      </div>
-    )
+  return (
+    <div className={Styles.novedadesCarousel}>
+      <Carousel
+        opts={{
+          align: "start",
+          loop: false
+        }}
+        className="max-w"
+      >
+        <CarouselContent>
+          {carouselItems.map((ci, index) => (
+            <CarouselItem key={index} className="md:basis-1/1 lg:basis-1/1" onClick={() => router.push(`/book/${ci.id}`)}>
+              <div className={Styles.novedadesBookCard}>
+                <img src={ci.image}/>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  )
 }
