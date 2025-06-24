2a06bba967cfb0f979a88ed223e3153a334ebe6f
Gaston <gastonpaez954@gmail.com>
2025-06-23 23:14:53 -0300

    review con id_book relation

--- backend/src/entidades/review.entity.ts
+++ backend/src/entidades/review.entity.ts
@@ -1,30 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, OneToOne, JoinColumn, ManyToOne} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, OneToOne, JoinColumn, ManyToOne } from 'typeorm';
 import { User } from './user.entity';
+import { Book } from './book.entity';
 
 @Entity('reviews')
-export class Review{
-    @ApiProperty({example: 1, description: 'ID Único de la Crítica'})
+export class Review {
+    @ApiProperty({ example: 1, description: 'ID Único de la Crítica' })
     @PrimaryGeneratedColumn()
     id: number;
 
-    @ApiProperty({example: 1, description: 'ID Único del Libro'})
-    @Column({name: 'id_book', type: 'integer' })
-    id_book: number;
-
-    @ApiProperty({example: 5, description: 'Puntaje del 1 al 5'})
-    @Column({type: 'smallint'})
+    @ApiProperty({ example: 5, description: 'Puntaje del 1 al 5' })
+    @Column({ type: 'smallint' })
     rating: number;
 
-    @ApiProperty({example: 'Muy Bueno', description: 'Comentario sobre el Libro'})
-    @Column({length: 350})
+    @ApiProperty({ example: 'Muy Bueno', description: 'Comentario sobre el Libro' })
+    @Column({ length: 350 })
     comment: string;
 
-    @ApiProperty({example: '05-06-2025', description: 'Fecha de la Publicacion de la Crítica'})
-    @Column({name: 'review_date', type: 'timestamp' })
+    @ApiProperty({ example: '05-06-2025', description: 'Fecha de la Publicacion de la Crítica' })
+    @Column({ name: 'review_date', type: 'timestamp' })
     reviewDate: Date
 
     @ManyToOne(() => User)
     @JoinColumn({ name: "id_user", referencedColumnName: "id" })
     user: User;
+
+    @ApiProperty({ description: 'Libro al que pertenece la crítica' })
+    @ManyToOne(() => Book, { eager: true, onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'id_book' })
+    book: Book;
+
 }
